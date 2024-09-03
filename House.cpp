ea72a8117c029b118c2bcdac6f62d54ea2cb75b1
DuyDo24 <a1921156@adelaide.edu.au>
2024-09-03 18:33:14 +0930

    2

---
+++ House.cpp
@@ -0,0 +1,36 @@
+#include "House.h"
+
+House::House(){
+    numAppliances = 0;
+    capacity = 0;
+    appliances = nullptr;
+}
+House::House(int numAppliances){
+    numAppliances = 0;
+    capacity = numAppliances;
+    appliances = new Appliance*[capacity];
+}
+bool House::addAppliance(Appliance* appliance) {
+    if (numAppliances < capacity) {
+        appliances[numAppliances++] = appliance;
+        return true;
+    }
+    return false;
+}
+
+double House::getTotalPowerConsumption() {
+    double totalPower = 0;
+    for (int i = 0; i < numAppliances; ++i) {
+        if (appliances[i] != nullptr) {
+            totalPower += appliances[i]->getPowerConsumption();
+        }
+    }
+    return totalPower;
+}
+
+House::~House() {
+    for (int i = 0; i < numAppliances; ++i) {
+        delete appliances[i]; // Deallocate each Appliance object
+    }
+    delete[] appliances; // Deallocate the array of pointers
+}
